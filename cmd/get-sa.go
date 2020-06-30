8c6301e1dec7bb1d7d015e7cb477e546a5d42389
KJ <info.j.landwoner.k@gmail.com>
2020-06-30 19:28:26 +0900

    Update flag

--- cmd/get-sa.go
+++ cmd/get-sa.go
@@ -16,18 +16,27 @@ limitations under the License.
 package cmd
 
 import (
-  "fmt"
-  "github.com/spf13/cobra"
+	"fmt"
+
+	"github.com/spf13/cobra"
 )
 
 func init() {
-  getCmd.AddCommand(getSaCmd)
+	getCmd.AddCommand(getSaCmd)
+	getSaCmd.PersistentFlags().StringVarP(&namespace, "namespace", "n", "", "namespace (default is in kubeconfig)")
+	getSaCmd.PersistentFlags().BoolVarP(&isAllNamespace, "all-namespaces", "A", false, "use all namespace (default is false)")
 }
 
-var getSaCmd = &cobra.Command{
-  Use:   "sa",
-  Short: "Get Service Accounts and PSP associated with it.",
-  Run: func(cmd *cobra.Command, args []string) {
-    fmt.Println("get sa")
-  },
-}
+var (
+	namespace      string
+	isAllNamespace bool
+	getSaCmd       = &cobra.Command{
+		Use:   "sa",
+		Short: "Get Service Accounts and PSP associated with it.",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("namespace %v\n", namespace)
+			fmt.Printf("isAllNamespace %v\n", isAllNamespace)
+			fmt.Println("get sa")
+		},
+	}
+)
