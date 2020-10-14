ee082fc2655c53d45e8fce4e19894259960140c9
Jinu <jlandowner8@gmail.com>
2020-10-14 16:27:02 +0900

    Add flags

--- cmd/list.go
+++ cmd/list.go
@@ -21,6 +21,7 @@ import (
 	"os"
 	"strconv"
 
+	"github.com/jlandowner/psp-util/cmd/options"
 	"github.com/jlandowner/psp-util/pkg/client"
 	"github.com/jlandowner/psp-util/pkg/printers"
 	"github.com/jlandowner/psp-util/pkg/relations"
@@ -29,60 +30,68 @@ import (
 
 func init() {
 	rootCmd.AddCommand(listCmd)
+	listCmd.Flags().BoolVarP(&l.All, "all", "v", false, "output all info")
+	listCmd.Flags().BoolVarP(&l.ClusterRole, "cluster-role", "cr", false, "output only clusterroles associated with PSP")
+	listCmd.Flags().BoolVarP(&l.Role, "role", "r", false, "output only roles associated with PSP")
 }
 
-var listCmd = &cobra.Command{
-	Use:   "list",
-	Short: "List PSPs and the related RBACs in cluster",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		ctx := context.Background()
-		k8sclient, err := client.NewClient(&kubeconfigPath)
-		if err != nil {
-			return fmt.Errorf("Failed to load kubeconfig %v: %v", kubeconfigPath, err.Error())
-		}
+var (
+	l = &options.ListOptions{}
 
-		psps, err := relations.GetRelationalPSPs(ctx, k8sclient)
-		if err != nil {
-			return err
-		}
-
-		w := printers.GetNewTabWriter(os.Stdout)
-		defer w.Flush()
-
-		printers.PrintLine(w, []string{"PSP-NAME", "CLUSTER-ROLE", "CLUSTER-ROLE-BINDING", "NS/ROLE", "NS/ROLE-BINDING", "PSP-UTIL-MANAGED"})
-
-		for _, psp := range psps {
-			if len(psp.ClusterRoles) == 0 && len(psp.Roles) == 0 {
-				printers.PrintLine(w, []string{psp.Name, "", "", "", "", ""})
-				continue
+	listCmd = &cobra.Command{
+		Use:               "list",
+		Short:             "List PSPs and the related RBACs in cluster",
+		PersistentPreRunE: l.PreRunE,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx := context.Background()
+			k8sclient, err := client.NewClient(&kubeconfigPath)
+			if err != nil {
+				return fmt.Errorf("Failed to load kubeconfig %v: %v", kubeconfigPath, err.Error())
 			}
-			// clusterrole can bind to either clusterrolebinding or rolebinding
-			for _, cr := range psp.ClusterRoles {
-				if len(cr.ClusterRoleBindings) == 0 && len(cr.RoleBindings) == 0 {
-					printers.PrintLine(w, []string{psp.Name, cr.Name, "", "", "", strconv.FormatBool(cr.IsManaged())})
+
+			psps, err := relations.GetRelationalPSPs(ctx, k8sclient)
+			if err != nil {
+				return err
+			}
+
+			w := printers.GetNewTabWriter(os.Stdout)
+			defer w.Flush()
+
+			printers.PrintLine(w, []string{"PSP", "ClusterRole", "ClusterRoleBinding", "NS/Role", "NS/RoleBinding", "PSP-UTIL-MANAGED"})
+
+			for _, psp := range psps {
+				if len(psp.ClusterRoles) == 0 && len(psp.Roles) == 0 {
+					printers.PrintLine(w, []string{psp.Name, "", "", "", "", ""})
 					continue
 				}
-				for _, crb := range cr.ClusterRoleBindings {
-					printers.PrintLine(w, []string{psp.Name, cr.Name, crb.Name, "", "", strconv.FormatBool(cr.IsManaged())})
+				// clusterrole can bind to either clusterrolebinding or rolebinding
+				for _, cr := range psp.ClusterRoles {
+					if len(cr.ClusterRoleBindings) == 0 && len(cr.RoleBindings) == 0 {
+						printers.PrintLine(w, []string{psp.Name, cr.Name, "", "", "", strconv.FormatBool(cr.IsManaged())})
+						continue
+					}
+					for _, crb := range cr.ClusterRoleBindings {
+						printers.PrintLine(w, []string{psp.Name, cr.Name, crb.Name, "", "", strconv.FormatBool(cr.IsManaged())})
+					}
+					for _, rb := range cr.RoleBindings {
+						rbname := fmt.Sprintf("%v/%v", rb.Namespace, rb.Name)
+						printers.PrintLine(w, []string{psp.Name, cr.Name, "", "", rbname, strconv.FormatBool(false)})
+					}
 				}
-				for _, rb := range cr.RoleBindings {
-					rbname := fmt.Sprintf("%v/%v", rb.Namespace, rb.Name)
-					printers.PrintLine(w, []string{psp.Name, cr.Name, "", "", rbname, strconv.FormatBool(false)})
+				// role can only bind to rolebinding
+				for _, r := range psp.Roles {
+					rname := fmt.Sprintf("%v/%v", r.Namespace, r.Name)
+					if len(r.RoleBindings) == 0 {
+						printers.PrintLine(w, []string{psp.Name, "", "", rname, "", strconv.FormatBool(false)})
+						continue
+					}
+					for _, rb := range r.RoleBindings {
+						rbname := fmt.Sprintf("%v/%v", r.Namespace, rb.Name)
+						printers.PrintLine(w, []string{psp.Name, "", "", rname, rbname, strconv.FormatBool(false)})
+					}
 				}
 			}
-			// role can only bind to rolebinding
-			for _, r := range psp.Roles {
-				rname := fmt.Sprintf("%v/%v", r.Namespace, r.Name)
-				if len(r.RoleBindings) == 0 {
-					printers.PrintLine(w, []string{psp.Name, "", "", rname, "", strconv.FormatBool(false)})
-					continue
-				}
-				for _, rb := range r.RoleBindings {
-					rbname := fmt.Sprintf("%v/%v", r.Namespace, rb.Name)
-					printers.PrintLine(w, []string{psp.Name, "", "", rname, rbname, strconv.FormatBool(false)})
-				}
-			}
-		}
-		return nil
-	},
-}
+			return nil
+		},
+	}
+)
